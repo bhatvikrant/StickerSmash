83caaed04aa76ff4b05df9c2a5d0f7192cfefe68
Vikrant Bhat <vik.bhat223@gmail.com>
2023-02-10 01:05:08 +0530

    chore: successfully implemented pan and tap gestures.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from "./components/Button";
 import ImageViewer from "./components/ImageViewer";
@@ -52,7 +53,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
@@ -91,7 +92,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
